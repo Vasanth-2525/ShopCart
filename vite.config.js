9c443fd0817329b8e474a04ae9f6759c3ec780c9
Vasanth-2525 <vasanthlogan2525@gmail.com>
2025-05-12 15:52:20 +0530

    Successfully

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-  ],
-  base:process.env.VITE_BASE_PATH || '/Vasanth-2525/ShopCart'
-})
+  tailwindcss()
+],
+  build: {
+    chunkSizeWarningLimit: 1000, // optional: increase if you want to suppress the warning
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          icons: ['react-icons'],
+          slick: ['react-slick', 'slick-carousel'],
+        },
+      },
+    },
+  },
+});
+
